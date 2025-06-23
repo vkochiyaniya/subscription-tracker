3572fd22c99d9558ef6bc89dae86ce68407a317b
vkochiyaniya <vkochiyaniya@users.noreply.github.com>
2025-06-23 17:31:29 +0000

    Add routes for auth, users, and subscriptions with CRUD operations

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import {PORT} from './config/env.js';
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
 
 app.get('/', (req, res)=>{
     res.send('Welcome to the home page!');
