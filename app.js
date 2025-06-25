d83b7b2e1b8b31f7672f591cbbf08607149b7a3f
vkochiyaniya <vkochiyaniya@users.noreply.github.com>
2025-06-25 16:25:39 +0000

    Add MongoDB Atlas integration with Mongoose and environment variables

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import {PORT} from './config/env.js';
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import authRouter from "./routes/auth.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -13,8 +14,10 @@ app.use('/api/v1/subscriptions', subscriptionRouter);
 app.get('/', (req, res)=>{
     res.send('Welcome to the home page!');
 });
-app.listen(PORT, () => {
+
+app.listen(PORT, async () => {
     console.log(`Listening on port http://localhost:${ PORT }`);
+    await connectToDatabase();
 });
 
 export default app;
