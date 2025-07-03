9a745c054c146732e6b63953803f306807a9f4a7
vkochiyaniya <vkochiyaniya@users.noreply.github.com>
2025-07-03 17:54:52 +0000

    Add Upstash reminder workflow for subscription deadlines

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import authRouter from "./routes/auth.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
+import workflowRouter from "./routes/workflow.routes.js";
 
 const app = express();
 
@@ -18,6 +19,8 @@ app.use(arcjetMiddleware);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/workflows', workflowRouter);
+
 
 app.use(errorMiddleware);
 
--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,4 +1,6 @@
 import Subscription from "../models/subscription.model.js";
+import { workflowClient } from '../config/upstash.js'
+import { SERVER_URL } from '../config/env.js'
 
 export const createSubscription = async (req, res, next) => {
     try {
@@ -7,7 +9,18 @@ export const createSubscription = async (req, res, next) => {
             users: req.user_id,
         });
 
-        res.status(201).send({sucess: true, data: subscription});
+        const { workflowRunId } = await workflowClient.trigger({
+            url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+            body: {
+                subscriptionId: subscription.id,
+            },
+            headers: {
+                'content-type': 'application/json',
+            },
+            retries: 0,
+        })
+
+        res.status(201).send({sucess: true, data: subscription, workflowRunId});
     }catch (e){
         next(e);
     }
